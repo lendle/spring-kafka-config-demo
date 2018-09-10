6e76a2a66752da9c343e07c0d22d520ca3ec4dcb
Sam Lendle <sam.lendle@gmail.com>
2018-09-10 11:11:45 -0700

    Add example

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Source;
+import org.springframework.context.annotation.Bean;
+import org.springframework.messaging.support.MessageBuilder;
+
+
+@SpringBootApplication
+@EnableBinding(Source.class)
+public class DemoApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @Bean
+    @Autowired
+    public CommandLineRunner commandLineRunner(Source source) {
+        return args -> source.output().send(MessageBuilder.withPayload(MyClass.newBuilder().setValue(100L).build()).build());
+    }
+
+}
